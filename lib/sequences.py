beaafc3aea3b94ae9d392ae31291154fc951186e
haniyare <hanihassanyare84@example.com>
2025-06-12 16:56:53 +0300

    labs-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    a, b = 0, 1
+
+    for _ in range(length):
+        fibonacci.append(a)
+        a, b = b, a + b
+
+    print(fibonacci)
